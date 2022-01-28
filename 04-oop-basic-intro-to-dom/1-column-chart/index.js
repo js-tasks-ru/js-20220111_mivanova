07f1de9c8a5153c2c22993e20fd6c6a0600cc059
m.ivanova <m.ivanova@xsolla.com>
2022-01-28 12:29:33 +0500

    task 5

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,82 +1,100 @@
 export default class ColumnChart {
-  constructor(columnsData) {
-    this.chartHeight = 50;
-    if (columnsData) {
-      this.formatHeading = columnsData.formatHeading;
-      this.data = columnsData.data;
-      this.label = columnsData.label;
-      this.value = this.formatHeading ? this.formatHeading(columnsData.value) : columnsData.value;
-      this.link = columnsData.link;
-      this.render();
-    } else {
-      this.renderWithoutData();
-    }
+  subElements = {};
+  chartHeight = 50;
+
+  constructor({
+    data = [],
+    label = '',
+    link = '',
+    value = 0,
+    formatHeading = data => data
+  } = {}) {
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.value = formatHeading(value);
+
+    this.render();
   }
 
-  getColumnProps (data = this.data) {
-    if (data) {
-      const maxValue = Math.max(...data);
-      const scale = 50 / maxValue;
-
-      return data.map(item => `<div style="--value: ${String(Math.floor(item * scale))}" data-tooltip="${(item / maxValue * 100).toFixed(0) + '%'}"></div>`);
-    }
-  }
-
-  getTemplate () {
+  get template() {
     return `
-      <div class="dashboard__chart_${this.label}">
-      <div class="column-chart" style="--chart-height: 50">
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
         <div class="column-chart__title">
           Total ${this.label}
-          <a href="${this.link}" class="column-chart__link">View all</a>
+          ${this.getLink()}
         </div>
         <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header">${this.value}</div>
+           <div data-element="header" class="column-chart__header">
+             ${this.value}
+           </div>
           <div data-element="body" class="column-chart__chart">
-            ${this.getColumnProps(this.data)?.join('')}
+            ${this.getColumnBody()}
           </div>
         </div>
       </div>
-    `
-  }
-
-  getEmptyTemplate () {
-    return `
-      <div class="column-chart column-chart_loading" style="--chart-height: 50">
-        <div class="column-chart__title">
-          Total ${this.label}
-          <a class="column-chart__link" href="${this.link}">View all</a>
-        </div>
-        <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header"></div>
-          <div data-element="body" class="column-chart__chart"></div>
-        </div>
-      </div>
-    `
-  }
-
-  renderWithoutData() {
-    const element = document.createElement('div');
-    element.innerHTML = this.getEmptyTemplate();
-    this.element = element.firstElementChild;
+    `;
   }
 
   render() {
     const element = document.createElement('div');
-    element.innerHTML = this.getTemplate();
+
+    element.innerHTML = this.template;
+
     this.element = element.firstElementChild;
+
+    if (this.data.length) {
+      this.element.classList.remove('column-chart_loading');
+    }
+
+    this.subElements = this.getSubElements();
   }
 
-  update (newData = this.data) {
-    return this.getColumnProps(newData);
+  getSubElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+
+      result[name] = subElement;
+    }
+
+    return result;
   }
 
-  remove () {
-    this.element?.remove();
+  getColumnBody() {
+    const maxValue = Math.max(...this.data);
+    const scale = this.chartHeight / maxValue;
+
+    return this.data
+      .map(item => {
+        const percent = (item / maxValue * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+      })
+      .join('');
+  }
+
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+  }
+
+  update(data) {
+    this.data = data;
+
+    this.subElements.body.innerHTML = this.getColumnBody(data);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
   }
 
   destroy() {
     this.remove();
+    this.element = null;
+    this.subElements = {};
   }
 }
-
